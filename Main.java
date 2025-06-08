0d4da529d8978cc3fa1ff21591bfe31a58c93247
Infinityavrk <infinityavrk@gmail.com>
2025-06-09 01:40:50 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class VulnerableLogin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String user = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String pass = scanner.nextLine();
+
+        try {
+            Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mydb", "root", "password");
+
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM users WHERE username = '" + user + "' AND password = '" + pass + "'";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            if (rs.next()) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Invalid credentials.");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        scanner.close();
+    }
+}
